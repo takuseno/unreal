a0e936cac26ce8b78c95dbaa77f163901d8c1dc3
takuseno <takuma.seno@gmail.com>
2018-08-07 19:38:35 +0900

    Fix random seed

--- train.py
+++ train.py
@@ -112,8 +112,8 @@ def train(server, cluster, args):
     local_init_op = tf.variables_initializer(local_vars)
 
     env = gym.make(args.env)
-    env.seed(args.index)
-    env = NoopResetEnv(env)
+    env.seed(constants.RANDOM_SEED)
+    env = NoopResetEnv(env, noop_max=7)
     env = EpisodicLifeEnv(env)
     wrapped_env = EnvWrapper(
         env,
